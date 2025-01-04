f5bfa58019905627981e430826c18bb0a9bab61e
Austin Dragone <18505590+AustinDrag1@users.noreply.github.com>
2025-01-03 21:06:50 -0500

    Created button based game with HTML result display (instead of console display) - need to fix event listener to ignroe parent

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
 // Randomize computer choice string
 function getComputerChoice()
 {
-
-    // Randomize choice
     const choice = Date.now() % 3;
-    let compChoice = choiceNumToStr(choice);
 
-    console.log("The computer chose " + compChoice);
-    computerChoice = compChoice;
-}
-
-// Turn number or string into choice string
-// Also prompt the computer to pick a choice and start a game
-function getHumanChoice( choice )
-{
-
-    if( typeof choice === "number" )
-    {
-        result = choiceNumToStr( choice );
-    }
-    else if( typeof choice === "string" )
-    {
-        result = choice.toLowerCase();
-    }
-
-    if( !["rock","paper","scissors"].includes(result) )
-    {
-        console.log("Your choice was not clear but it looks close enough to rock");
-        result = "rock";
-    }
-
-    console.log("You chose " + result);
-    humanChoice = result;
-
-    getComputerChoice();
-    playGame();
-}
-
-// Turn number into choice
-function choiceNumToStr( choice )
-{
     if( choice === 0 ) { result = "rock"; }
     else if( choice === 1 ) { result = "paper"; }
     else if( choice === 2 ) { result = "scissors"; }
@@ -48,9 +11,12 @@ function choiceNumToStr( choice )
     return result;
 }
 
-// Compare choices and award points if a winner
-function playRound( humanChoice, computerChoice )
+// Compare choices, award points if a winner, and update result element
+function playRound( e )
 {
+    // Confirm selection
+    const humanChoice = e.target.name;
+    const computerChoice = getComputerChoice();
     let result;
 
     // Compare choices
@@ -75,43 +41,43 @@ function playRound( humanChoice, computerChoice )
     }
 
     // Award points
+    if( result === "win" ) { humanScore++; }
+    else if( result === "loss" ) { computerScore++; }
+
+    updateResultDisplay(result, humanChoice, computerChoice);
+}
+
+// Displays the game result and the score
+function updateResultDisplay( result, humanChoice, computerChoice )
+{
+    let display = "<p>"
+
     if( result === "win" )
     {
-        humanScore++;
-        console.log("You win!");
+        display = "You won! (" + humanChoice + " beats " + computerChoice + ")</p>";
+
     }
     else if( result === "loss" )
     {
-        computerScore++;
-        console.log("You lose!");
+        display = "You lost (" + computerChoice + " beats " + humanChoice + ")</p>";
+
     }
     else
     {
-        console.log("There was a tie.")
-    }
-}
-
-function playGame()
-{
-    playRound(humanChoice, computerChoice);
-
-    if( humanScore === 5 )
-    {
-        console.log("You won 5 times! Points have been reset.");
-        humanScore = 0;
-        computerScore = 0;
+        display = "It's a tie (you both chose " + computerChoice + ")</p>";
     }
 
-    if( computerScore === 5 )
-    {
-        console.log("You lost 5 times! Points have been reset.");
-        humanScore = 0;
-        computerScore = 0;
-    }
+    display += "<p>You: " + humanScore + "</p><p>Computer: " + computerScore + "</p>";
+
+    resultDisplay.innerHTML = display;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-let humanChoice;
-let computerChoice;
+const optionList = document.querySelector("#option-list");
+optionList.addEventListener("click", (e) => {
+    playRound(e);
+});
+
+const resultDisplay = document.querySelector("#result-display");
